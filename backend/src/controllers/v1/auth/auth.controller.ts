d4e5083847fcdc68bffe6996aee923ba72926553
Vyacheslav <hikkathon@gmail.com>
2025-06-19 18:14:15 +0300

    Переделал валидацию. (#1)

--- backend/src/controllers/v1/auth/auth.controller.ts
+++ backend/src/controllers/v1/auth/auth.controller.ts
@@ -1,36 +1,22 @@
 import {Request, Response, NextFunction} from 'express';
-import {validationResult} from 'express-validator';
 import * as authService from '../../../services/auth.service';
 import {ApiResponseBuilder} from '../../../utils/apiResponse';
 import {getErrorMessage} from "../../../utils/getErrorMessage";
 
-export const registration = async (req: Request, res: Response, next: NextFunction) => {
+export const registration = async (req: Request, res: Response) => {
     try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            ApiResponseBuilder.error(
-                res,
-                'USER_VALIDATION_FAILED',
-                'Failed to validation user',
-                401,
-                getErrorMessage(errors)
-            );
-        }
-
         const {email, password} = req.body;
-        const candidate = await authService.register(email, password);
-        res.cookie('refreshToken', candidate.tokens.refreshToken, {
-            maxAge: 30 * 24 * 60 * 60 * 1000,
-            httpOnly: true,
-            secure: true
-        });
-        ApiResponseBuilder.success(res, candidate, 200);
+        const user = await authService.register(email, password);
+
+        authService.setAuthCookies(res, user.tokens.refreshToken);
+
+        ApiResponseBuilder.success(res, user, 200);
     } catch (error) {
         ApiResponseBuilder.error(
             res,
             'USER_REGISTRATION_FAILED',
             'Failed to registration user',
-            500,
+            400,
             getErrorMessage(error)
         );
     }
@@ -52,13 +38,21 @@ export const logout = async (req: Request, res: Response, next: NextFunction) =>
     }
 };
 
-export const activate = async (req: Request, res: Response, next: NextFunction) => {
+export const activate = async (req: Request, res: Response) => {
     try {
         const activateLink = req.params.link;
+
         await authService.activate(activateLink);
-        res.status(200).json({ok: true});
+
+        ApiResponseBuilder.success(res, "Account activation was successful", 200, { link: activateLink });
     } catch (error) {
-        next(error);
+        ApiResponseBuilder.error(
+            res,
+            'USER_ACTIVATION_FAILED',
+            'Failed to activation user',
+            400,
+            getErrorMessage(error)
+        );
     }
 };
 
