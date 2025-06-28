ca36eb16c1f496ed28b832ce28ae65e5e906c95b
v.kuzmin <hikkathon@gmail.com>
2025-06-28 15:52:45 +0300

    init frontend

--- backend/src/routes/v1/auth.routes.ts
+++ backend/src/routes/v1/auth.routes.ts
@@ -8,7 +8,7 @@ const router = Router();
 router.post('/registration', authValidator.auth(), validate, authController.registration);
 router.post('/login', authValidator.auth(), validate, authController.login);
 router.post('/logout', authController.logout);
-router.post('/refresh-token', authController.refreshToken);
+router.get('/refresh-token', authController.refreshToken);
 router.get('/activate/:link', authValidator.activation(), validate, authController.activate);
 
 export default router;
